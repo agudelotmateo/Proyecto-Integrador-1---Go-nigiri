d04bf05a5be4e8083e31b94a29fd2f07309df607
wsalasg@eafit.edu.co <wsalasg@eafit.edu.com>
2017-04-27 14:15:13 -0500

    Correcion de los nombres en los diccionarios

--- app/src/main/java/com/aprendamosjapones/aprendedamosjapones/DictionaryHiragana/DictionaryHiraganaActivity.java
+++ app/src/main/java/com/aprendamosjapones/aprendedamosjapones/DictionaryHiragana/DictionaryHiraganaActivity.java
@@ -2,6 +2,8 @@ package com.aprendamosjapones.aprendedamosjapones.DictionaryHiragana;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.AbsListView;
 import android.widget.GridView;
 
 import com.aprendamosjapones.aprendedamosjapones.R;
@@ -12,128 +14,157 @@ public class DictionaryHiraganaActivity extends Activity {
     /** Called when the activity is first created. */
 	
 	private DictionaryHiraganaAdapter mAdapter;
-	private ArrayList<String> listCountry;
-	private ArrayList<Integer> listFlag;
+    private DictionaryHiraganaAdapter mAdapter2;
+	private ArrayList<String> listNameHiragana5;
+	private ArrayList<Integer> listImagesHiragana5;
+    private ArrayList<String> listNameHiragana3;
+    private ArrayList<Integer> listImagesHiragana3;
 	
 	private GridView gridView;
-	
+    private GridView gridView2;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dictionary_hiragana_grid);
         
         prepareList();
+        prepareList2();
         
         // prepared arraylist and passed it to the Adapter class
-        mAdapter = new DictionaryHiraganaAdapter(this,listCountry, listFlag);
+        mAdapter = new DictionaryHiraganaAdapter(this,listNameHiragana5, listImagesHiragana5);
+
+        mAdapter2 = new DictionaryHiraganaAdapter(this,listNameHiragana3, listImagesHiragana3);
         
         // Set custom adapter to gridview
-        gridView = (GridView) findViewById(R.id.gridView2);
+        gridView = (GridView) findViewById(R.id.gridView1);
         gridView.setAdapter(mAdapter);
+
+        gridView2 = (GridView) findViewById(R.id.gridView2);
+        gridView2.setAdapter(mAdapter2);
         
     }
 
     public void prepareList()
     {
-        listCountry = new ArrayList<String>();
+        listNameHiragana5 = new ArrayList<String>();
 
-        listCountry.add("a");
-        listCountry.add("i");
-        listCountry.add("u");
-        listCountry.add("e");
-        listCountry.add("o");
-        listCountry.add("ka");
-        listCountry.add("ki");
-        listCountry.add("ku");
-        listCountry.add("ke");
-        listCountry.add("ko");
-        listCountry.add("sa");
-        listCountry.add("shi");
-        listCountry.add("su");
-        listCountry.add("se");
-        listCountry.add("so");
-        listCountry.add("ta");
-        listCountry.add("chi");
-        listCountry.add("tsu");
-        listCountry.add("te");
-        listCountry.add("to");
-        listCountry.add("na");
-        listCountry.add("ni");
-        listCountry.add("nu");
-        listCountry.add("ne");
-        listCountry.add("ha");
-        listCountry.add("hi");
-        listCountry.add("fu");
-        listCountry.add("he");
-        listCountry.add("ho");
-        listCountry.add("ma");
-        listCountry.add("mi");
-        listCountry.add("mu");
-        listCountry.add("me");
-        listCountry.add("mo");
-        listCountry.add("ya");
-        listCountry.add("yu");
-        listCountry.add("yo");
-        listCountry.add("ra");
-        listCountry.add("ri");
-        listCountry.add("re");
-        listCountry.add("ru");
-        listCountry.add("re");
-        listCountry.add("ro");
-        listCountry.add("wa");
-        listCountry.add("");
-        listCountry.add("wo");
-        listCountry.add("n");
+        listNameHiragana5.add("a");
+        listNameHiragana5.add("i");
+        listNameHiragana5.add("u");
+        listNameHiragana5.add("e");
+        listNameHiragana5.add("o");
+        listNameHiragana5.add("ka");
+        listNameHiragana5.add("ki");
+        listNameHiragana5.add("ku");
+        listNameHiragana5.add("ke");
+        listNameHiragana5.add("ko");
+        listNameHiragana5.add("sa");
+        listNameHiragana5.add("shi");
+        listNameHiragana5.add("su");
+        listNameHiragana5.add("se");
+        listNameHiragana5.add("so");
+        listNameHiragana5.add("ta");
+        listNameHiragana5.add("chi");
+        listNameHiragana5.add("tsu");
+        listNameHiragana5.add("te");
+        listNameHiragana5.add("to");
+        listNameHiragana5.add("na");
+        listNameHiragana5.add("ni");
+        listNameHiragana5.add("nu");
+        listNameHiragana5.add("ne");
+        listNameHiragana5.add("ha");
+        listNameHiragana5.add("hi");
+        listNameHiragana5.add("fu");
+        listNameHiragana5.add("he");
+        listNameHiragana5.add("ho");
+        listNameHiragana5.add("ma");
+        listNameHiragana5.add("mi");
+        listNameHiragana5.add("mu");
+        listNameHiragana5.add("me");
+        listNameHiragana5.add("mo");
+        listNameHiragana5.add("ya");
+        listNameHiragana5.add("yu");
+        listNameHiragana5.add("yo");
+        listNameHiragana5.add("ra");
+        listNameHiragana5.add("ri");
+        listNameHiragana5.add("re");
+        listNameHiragana5.add("ru");
+        listNameHiragana5.add("re");
+        listNameHiragana5.add("ro");
+        listNameHiragana5.add("wa");
+        listNameHiragana5.add("");
+        listNameHiragana5.add("wo");
+        listNameHiragana5.add("n");
 
 
-        listFlag = new ArrayList<Integer>();
+        listImagesHiragana5 = new ArrayList<Integer>();
 
-        listFlag.add(R.drawable.hiragana1a);
-        listFlag.add(R.drawable.hiragana2i);
-        listFlag.add(R.drawable.hiragana3u);
-        listFlag.add(R.drawable.hiragana4e);
-        listFlag.add(R.drawable.hiragana5o);
-        listFlag.add(R.drawable.hiragana6ka);
-        listFlag.add(R.drawable.hiragana7ki);
-        listFlag.add(R.drawable.hiragana8ku);
-        listFlag.add(R.drawable.hiragana9ke);
-        listFlag.add(R.drawable.hiragana10ko);
-        listFlag.add(R.drawable.hiragana11sa);
-        listFlag.add(R.drawable.hiragana12shi);
-        listFlag.add(R.drawable.hiragana13su);
-        listFlag.add(R.drawable.hiragana14se);
-        listFlag.add(R.drawable.hiragana15so);
-        listFlag.add(R.drawable.hiragana16ta);
-        listFlag.add(R.drawable.hiragana17chi);
-        listFlag.add(R.drawable.hiragana18tsu);
-        listFlag.add(R.drawable.hiragana19te);
-        listFlag.add(R.drawable.hiragana20to);
-        listFlag.add(R.drawable.hiragana21na);
-        listFlag.add(R.drawable.hiragana22ni);
-        listFlag.add(R.drawable.hiragana23nu);
-        listFlag.add(R.drawable.hiragana24ne);
-        listFlag.add(R.drawable.hiragana25no);
-        listFlag.add(R.drawable.hiragana26ha);
-        listFlag.add(R.drawable.hiragana27hi);
-        listFlag.add(R.drawable.hiragana28fu);
-        listFlag.add(R.drawable.hiragana29he);
-        listFlag.add(R.drawable.hiragana30ho);
-        listFlag.add(R.drawable.hiragana31ma);
-        listFlag.add(R.drawable.hiragana32mi);
-        listFlag.add(R.drawable.hiragana33mu);
-        listFlag.add(R.drawable.hiragana34me);
-        listFlag.add(R.drawable.hiragana35mo);
-        listFlag.add(R.drawable.hiragana36ya);
-        listFlag.add(R.drawable.hiragana37yu);
-        listFlag.add(R.drawable.hiragana38yo);
-        listFlag.add(R.drawable.hiragana39ra);
-        listFlag.add(R.drawable.hiragana40ri);
-        listFlag.add(R.drawable.hiragana41ru);
-        listFlag.add(R.drawable.hiragana42re);
-        listFlag.add(R.drawable.hiragana43ro);
-        listFlag.add(R.drawable.hiragana44wa);
-        listFlag.add(R.drawable.blanco);
-        listFlag.add(R.drawable.hiragana45wo);
-        listFlag.add(R.drawable.hiragana46n);
+        listImagesHiragana5.add(R.drawable.hiragana1a);
+        listImagesHiragana5.add(R.drawable.hiragana2i);
+        listImagesHiragana5.add(R.drawable.hiragana3u);
+        listImagesHiragana5.add(R.drawable.hiragana4e);
+        listImagesHiragana5.add(R.drawable.hiragana5o);
+        listImagesHiragana5.add(R.drawable.hiragana6ka);
+        listImagesHiragana5.add(R.drawable.hiragana7ki);
+        listImagesHiragana5.add(R.drawable.hiragana8ku);
+        listImagesHiragana5.add(R.drawable.hiragana9ke);
+        listImagesHiragana5.add(R.drawable.hiragana10ko);
+        listImagesHiragana5.add(R.drawable.hiragana11sa);
+        listImagesHiragana5.add(R.drawable.hiragana12shi);
+        listImagesHiragana5.add(R.drawable.hiragana13su);
+        listImagesHiragana5.add(R.drawable.hiragana14se);
+        listImagesHiragana5.add(R.drawable.hiragana15so);
+        listImagesHiragana5.add(R.drawable.hiragana16ta);
+        listImagesHiragana5.add(R.drawable.hiragana17chi);
+        listImagesHiragana5.add(R.drawable.hiragana18tsu);
+        listImagesHiragana5.add(R.drawable.hiragana19te);
+        listImagesHiragana5.add(R.drawable.hiragana20to);
+        listImagesHiragana5.add(R.drawable.hiragana21na);
+        listImagesHiragana5.add(R.drawable.hiragana22ni);
+        listImagesHiragana5.add(R.drawable.hiragana23nu);
+        listImagesHiragana5.add(R.drawable.hiragana24ne);
+        listImagesHiragana5.add(R.drawable.hiragana25no);
+        listImagesHiragana5.add(R.drawable.hiragana26ha);
+        listImagesHiragana5.add(R.drawable.hiragana27hi);
+        listImagesHiragana5.add(R.drawable.hiragana28fu);
+        listImagesHiragana5.add(R.drawable.hiragana29he);
+        listImagesHiragana5.add(R.drawable.hiragana30ho);
+        listImagesHiragana5.add(R.drawable.hiragana31ma);
+        listImagesHiragana5.add(R.drawable.hiragana32mi);
+        listImagesHiragana5.add(R.drawable.hiragana33mu);
+        listImagesHiragana5.add(R.drawable.hiragana34me);
+        listImagesHiragana5.add(R.drawable.hiragana35mo);
+        listImagesHiragana5.add(R.drawable.hiragana36ya);
+        listImagesHiragana5.add(R.drawable.hiragana37yu);
+        listImagesHiragana5.add(R.drawable.hiragana38yo);
+        listImagesHiragana5.add(R.drawable.hiragana39ra);
+        listImagesHiragana5.add(R.drawable.hiragana40ri);
+        listImagesHiragana5.add(R.drawable.hiragana41ru);
+        listImagesHiragana5.add(R.drawable.hiragana42re);
+        listImagesHiragana5.add(R.drawable.hiragana43ro);
+        listImagesHiragana5.add(R.drawable.hiragana44wa);
+        listImagesHiragana5.add(R.drawable.blanco);
+        listImagesHiragana5.add(R.drawable.hiragana45wo);
+        listImagesHiragana5.add(R.drawable.hiragana46n);
     }
+
+    public void prepareList2() {
+
+        listNameHiragana3 = new ArrayList<String>();
+
+        listNameHiragana3.add("a");
+        listNameHiragana3.add("i");
+        listNameHiragana3.add("a");
+        listNameHiragana3.add("i");
+
+        listImagesHiragana3 = new ArrayList<Integer>();
+
+        listImagesHiragana3.add(R.drawable.hiragana1a);
+        listImagesHiragana3.add(R.drawable.hiragana2i);
+        listImagesHiragana3.add(R.drawable.hiragana2i);
+        listImagesHiragana3.add(R.drawable.hiragana2i);
+    }
+
+
 }
