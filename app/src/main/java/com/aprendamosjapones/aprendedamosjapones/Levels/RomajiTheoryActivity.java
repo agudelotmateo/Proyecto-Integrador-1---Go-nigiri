bfb30ec9ef15788df4ea94818cec80d7c488d3b2
wsalasg@eafit.edu.co <wsalasg@eafit.edu.com>
2017-04-29 17:26:25 -0500

    Correciones en los niveles de katakana e hiragana que permiten hacer scroll vertical sobre los mismos

--- app/src/main/java/com/aprendamosjapones/aprendedamosjapones/Levels/RomajiTheoryActivity.java
+++ app/src/main/java/com/aprendamosjapones/aprendedamosjapones/Levels/RomajiTheoryActivity.java
@@ -1,8 +1,20 @@
 package com.aprendamosjapones.aprendedamosjapones.Levels;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aprendamosjapones.aprendedamosjapones.R;
+
 /**
  * Created by catalina on 27/04/17.
  */
 
-public class RomajiTheoryActivity {
+public class RomajiTheoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
