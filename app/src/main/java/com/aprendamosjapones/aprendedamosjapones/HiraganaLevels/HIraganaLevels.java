63b47c516c39b3496d43e6a991572314789f6e52
catalina <catapf493@yahoo.com>
2017-05-11 13:11:22 -0500

    Se modifica la teoría de ¿Qué es el kanji?

--- app/src/main/java/com/aprendamosjapones/aprendedamosjapones/HiraganaLevels/HIraganaLevels.java
+++ app/src/main/java/com/aprendamosjapones/aprendedamosjapones/HiraganaLevels/HIraganaLevels.java
@@ -14,7 +14,7 @@ public class HIraganaLevels {
                             " (nuestro alfabeto). Aunque no es un sistema de escritura oficial del japonés," +
                             " es posible encontrarlo en algunas señales, marcas o similares en Japón. De hecho" +
                             " nosotros conocemos algunas palabras escritas en rōmaji: sushi, karate, Suzuki," +
-                            " entre otras.\n" +
+                            " entre otras.\n\n" +
                             "El uso del rōmaji puede facilitar nuestra tarea de aprender el idioma dado" +
                             " que tratamos un alfabeto más familiar a nosotros, y por este motivo en las" +
                             " primeras lecciones, aquellas para aprender hiragana y katakana, lo veremos " +
@@ -26,7 +26,7 @@ public class HIraganaLevels {
             new HiraganaLevel("¿Qué es el hiragana?",
                     "El hiragana es uno de los sistemas oficiales de escritura del japonés. Es como el alfabeto " +
                             "para nosotros en el sentido de que nos define la fonética o sonidos del lenguaje, " +
-                            "la diferencia es que más que un alfabeto es un silabario.\n" +
+                            "la diferencia es que más que un alfabeto es un silabario.\n\n" +
                             "El silabario está compuesto por 46 caracteres básicos y es usado para palabras " +
                             "nativas de Japón y elementos gramaticales.\n", "","","", R.drawable.hiragana),
             new HiraganaLevel("Las vocales y la n (ん)",
@@ -34,7 +34,7 @@ public class HIraganaLevels {
                             "utilizamos, la única diferencia es el orden en el que se presentan: a, i, u, e, o. " +
                             "Y su pronunciación no cambia para un hablante de la lengua hispana. Eso sí, ten en " +
                             "cuenta que la 'u' se pronuncia más suave, relaja los labios cuando la pronuncies en" +
-                            " vez de cerrarlos formando una o, como normalmente hacemos.\n" +
+                            " vez de cerrarlos formando una o, como normalmente hacemos.\n\n" +
                             "Hay momentos en los que la 'u' y la 'i' no se pronuncian. Esto se suele dar cuando " +
                             "se encuentran entre las consonantes: 'p', 't', 'k', 'ch', 'f', 'h', 's', 'sh'. O" +
                             " también cuando se encuentran al final de la palabra y son precedidas por alguna" +
--- app/src/main/java/com/aprendamosjapones/aprendedamosjapones/KanjiLevels/KanjiLevels.java
+++ app/src/main/java/com/aprendamosjapones/aprendedamosjapones/KanjiLevels/KanjiLevels.java
@@ -8,9 +8,22 @@ import java.util.List;
 
 public class KanjiLevels {
     private static KanjiLevel[] levels = {
-            new KanjiLevel("¿Que es el Kanji?",
-                    "Los kanjis (漢字 kanji?, literalmente «carácter han») son los sinogramas utilizados en la" +
-                            " escritura del idioma japonés.", R.drawable.kanji),
+            new KanjiLevel("¿Qué es el Kanji?",
+                    "El kanji es un sistema de escritura originario de China, " +
+                            " por este motivo es posible que se te haga tan parecida la escritura " +
+                            " de ambos idiomas. Sin embargo, puedes encontrar diferencias entre el " +
+                            " kanji japonés y hanzi chino, resultado del transcurso del tiempo.\n\n" +
+                            "Los caracteres del kanji a diferencia del hiragana y del katakana no" +
+                            " representan sonidos del idioma sino que representan una idea completa," +
+                            " es decir, son ideogramas. Por este motivo pueden existir una gran" +
+                            " variedad de estos caracteres, y aprenderlos todos no es una tarea" +
+                            " fácil. De hecho hay más de 7000 pero un adulto promedio sólo" +
+                            " conoce alrededor de 2000.\n\n" +
+                            "Lastimosamente, por el momento sólo aprenderemos 80 kanjis," +
+                            " los más básicos y los que necesitas conocer para presentar" +
+                            " el nivel 5 del examen de certificación de japonés: JLPT. Ten" +
+                            " en cuenta que son 5 niveles, donde el quinto es el más básico" +
+                            " de todos.", R.drawable.kanji),
     };
 
     public static List<KanjiLevel> getCourses() {
