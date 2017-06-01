6ae5eea6027c0368ed0812c0d0d9a144aa3322eb
agudelotmateo <agudelotmateo@gmail.com>
2017-06-01 11:43:40 -0500

    Agregado menu Match it

---
+++ app/src/main/java/com/aprendamosjapones/aprendedamosjapones/Games/MemoryGame/matching/InstructionsMatchit.java
@@ -0,0 +1,18 @@
+package com.aprendamosjapones.aprendedamosjapones.Games.MemoryGame.matching;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aprendamosjapones.aprendedamosjapones.R;
+
+/**
+ * Created by mateo on 6/1/17.
+ */
+
+public class InstructionsMatchit extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instruction_matchit_game);
+    }
+}
