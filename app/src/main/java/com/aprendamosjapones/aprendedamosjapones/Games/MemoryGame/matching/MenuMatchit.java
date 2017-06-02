3d334454d1cf599c3ef4e14919e44db5b3925239
wsalasg@eafit.edu.co <wsalasg@eafit.edu.com>
2017-06-01 20:19:33 -0500

    Se agregan mas up button

--- app/src/main/java/com/aprendamosjapones/aprendedamosjapones/Games/MemoryGame/matching/MenuMatchit.java
+++ app/src/main/java/com/aprendamosjapones/aprendedamosjapones/Games/MemoryGame/matching/MenuMatchit.java
@@ -3,6 +3,7 @@ package com.aprendamosjapones.aprendedamosjapones.Games.MemoryGame.matching;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -22,6 +23,8 @@ public class MenuMatchit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matchit_menu);
+
+        setToolbar(); // Reemplazar toolbar
 
         goInstructionsMatchit = (Button)findViewById(R.id.instructionsMatchit);
         goInstructionsMatchit.setOnClickListener(new View.OnClickListener() {
@@ -126,4 +129,12 @@ public class MenuMatchit extends AppCompatActivity {
             }
         });
     }
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null)// Habilitar Up Button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
 }
